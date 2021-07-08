1e99343673c84eacaef4232bbe6da913181fd34f
jayasindhura <jpailla@unomaha.edu>
2021-07-07 19:21:57 -0500

    First commit

---
+++ portfolio/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url
+from . import views
+from django.urls import path
+
+app_name = 'portfolio'
+urlpatterns = [
+    path('', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
+    path('customer_list', views.customer_list, name='customer_list'),
+    path('customer/<int:pk>/edit/', views.customer_edit, name='customer_edit'),
+    path('customer/<int:pk>/delete/', views.customer_delete, name='customer_delete'),
+
+    path('stock_list', views.stock_list, name='stock_list'),
+    path('stock/create/', views.stock_new, name='stock_new'),
+    path('stock/<int:pk>/edit/', views.stock_edit, name='stock_edit'),
+    path('stock/<int:pk>/delete/', views.stock_delete, name='stock_delete'),
+
+    path('investment_list', views.investment_list, name='investment_list'),
+    path('investment/create/', views.investment_new, name='investment_new'),
+    path('investment/<int:pk>/edit/', views.investment_edit, name='investment_edit'),
+    path('investment/<int:pk>/delete/', views.investment_delete, name='investment_delete'),
+
+    path('fund_list', views.fund_list, name='fund_list'),
+    path('fund/create/', views.fund_new, name='fund_new'),
+    path('fund/<int:pk>/edit/', views.fund_edit, name='fund_edit'),
+    path('fund/<int:pk>/delete/', views.fund_delete, name='fund_delete'),
+
+    path('signup/', views.signup_view, name="signup")
+]
